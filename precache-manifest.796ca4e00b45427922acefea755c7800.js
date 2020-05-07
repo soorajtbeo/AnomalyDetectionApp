d134533009c7d7d58a25266d1836ba05df6e54b3
unknown <sooraj.t@beo.in>
2020-05-07 10:57:20 +0530

    Updates

---
+++ precache-manifest.796ca4e00b45427922acefea755c7800.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eae0e7ce1f606cf98e18da75e7a0552",
+    "url": "/AnomalyDetectionApp/index.html"
+  },
+  {
+    "revision": "97f08facad3ab2633b5b",
+    "url": "/AnomalyDetectionApp/static/css/main.a7247a1e.chunk.css"
+  },
+  {
+    "revision": "c4a6f2de266e0084c2b5",
+    "url": "/AnomalyDetectionApp/static/js/2.c1e583e6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/AnomalyDetectionApp/static/js/2.c1e583e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97f08facad3ab2633b5b",
+    "url": "/AnomalyDetectionApp/static/js/main.b8494112.chunk.js"
+  },
+  {
+    "revision": "67c1cf2228ac501ec936",
+    "url": "/AnomalyDetectionApp/static/js/runtime-main.adacfc1a.js"
+  },
+  {
+    "revision": "785d0e172757cf543eb7fbf6d4de2a2a",
+    "url": "/AnomalyDetectionApp/static/media/Helvetica25UltraLight_22433.785d0e17.woff"
+  },
+  {
+    "revision": "6b2a3d8bb1e2ad02806df770bac826c2",
+    "url": "/AnomalyDetectionApp/static/media/Helvetica26UltraLightItalic_22434.6b2a3d8b.woff"
+  },
+  {
+    "revision": "b7061f63064b0cc4f624b0d7e2a56d48",
+    "url": "/AnomalyDetectionApp/static/media/Helvetica35Thin_22435.b7061f63.woff"
+  },
+  {
+    "revision": "904b466b64ca353196ddecce50007015",
+    "url": "/AnomalyDetectionApp/static/media/Helvetica36ThinItalic_22436.904b466b.woff"
+  },
+  {
+    "revision": "3638bbe2e94d313ef0f066189dfc808f",
+    "url": "/AnomalyDetectionApp/static/media/Helvetica45Light_22437.3638bbe2.woff"
+  },
+  {
+    "revision": "cf9d649388f51895cd4b32b66371d30e",
+    "url": "/AnomalyDetectionApp/static/media/Helvetica46LightItalic_22438.cf9d6493.woff"
+  },
+  {
+    "revision": "82d4c90ce715d4519969953f1dbb2275",
+    "url": "/AnomalyDetectionApp/static/media/Helvetica55Roman_22439.82d4c90c.woff"
+  },
+  {
+    "revision": "585da360be43fc0f4735bb1d9602c46b",
+    "url": "/AnomalyDetectionApp/static/media/Helvetica56Italic_22440.585da360.woff"
+  },
+  {
+    "revision": "ef372a1621c497cbdb21806f15719519",
+    "url": "/AnomalyDetectionApp/static/media/Helvetica65Medium_22443.ef372a16.woff"
+  },
+  {
+    "revision": "b571b5956d33e748583a9b2030697e82",
+    "url": "/AnomalyDetectionApp/static/media/Helvetica76BoldItalic.b571b595.woff"
+  },
+  {
+    "revision": "0383a7cf000daa41a83e852e89114ac6",
+    "url": "/AnomalyDetectionApp/static/media/Helvetica85Heavy_22449.0383a7cf.woff"
+  },
+  {
+    "revision": "50c3b6b0f52467d77612d0cc218e6f7a",
+    "url": "/AnomalyDetectionApp/static/media/Helvetica86HeavyItalic_22450.50c3b6b0.woff"
+  },
+  {
+    "revision": "33b2cd4cdd4c6a161684df7f7873a1df",
+    "url": "/AnomalyDetectionApp/static/media/Helvetica95Black.33b2cd4c.woff"
+  },
+  {
+    "revision": "901b58937580b50dfcfab6b4c7711404",
+    "url": "/AnomalyDetectionApp/static/media/HelveticaNeueBlackItalic 1.901b5893.woff"
+  },
+  {
+    "revision": "190b4e095db9510029445bb90080aa77",
+    "url": "/AnomalyDetectionApp/static/media/HelveticaNeueBold.190b4e09.woff"
+  },
+  {
+    "revision": "9a9a6ae7c5ecadd780246eecfe86f9e9",
+    "url": "/AnomalyDetectionApp/static/media/HelveticaNeueMediumItalic.9a9a6ae7.woff"
+  },
+  {
+    "revision": "e7e22ffb65dbe62df198be8d299b27c7",
+    "url": "/AnomalyDetectionApp/static/media/machine.e7e22ffb.jpg"
+  }
+]);
